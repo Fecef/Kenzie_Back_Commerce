97545017803adc34091cab4234a5358a86af4ef0
Guilherme <guilhermemachado755@gmail.com>
2023-03-06 16:42:40 -0300

    Add:model, serializer and views

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .models import Order
+from .serializers import OrderSerializer
+from .utils import update_stock
 
-# Create your views here.
+
+class OrderList(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def perform_create(self, serializer):
+        order = serializer.save(user=self.request.user)
+        product = order.product.all()
+        quantities = [1] * len(product)
+        update_stock(product, quantities)
+        return serializer.save(user=self.request.user)
+
+
+class OrderDeatil(generics.RetrieveUpdateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def perform_update(self, serializer):
+        return serializer.save(vendedor=self.request.user)
