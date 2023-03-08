10506c0d2375d62efde970afc39e2ffa00872bb2
Fecef <fecef.figueiredo@gmail.com>
2023-03-08 15:52:05 -0300

    feat: add post product.

--- user/serializers.py
+++ user/serializers.py
@@ -5,6 +5,12 @@ from datetime import datetime
 from .models import User
 
 
+class UserTrackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "first_name", "last_name", "email"]
+
+
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=127, write_only=True)
     created_at = serializers.SerializerMethodField()
