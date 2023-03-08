9fae36dcb4a7d56208beb72983e06bbe5b4d6c15
Fecef <fecef.figueiredo@gmail.com>
2023-03-07 22:56:20 -0300

    feat: add address post/patch route.

--- user/serializers.py
+++ user/serializers.py
@@ -5,6 +5,9 @@ from datetime import datetime
 from .models import User
 
 
+
+
+
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=127, write_only=True)
     created_at = serializers.SerializerMethodField()
