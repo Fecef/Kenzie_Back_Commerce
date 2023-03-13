2cf81292ad005fc824342c3c52ed2c12151cef42
Elias <e.borges289@gmail.com>
2023-03-13 12:07:24 -0300

    add field of avaliable in products

--- products/models.py
+++ products/models.py
@@ -9,6 +9,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     category = models.CharField(max_length=50)
     current_inventory = models.PositiveSmallIntegerField()
+    is_avaliable = models.BooleanField(default=True)
 
     added_by = models.ForeignKey(
         "user.User", on_delete=models.CASCADE, related_name="products"
