44a77314b2882bc2a04b4b3c40772a50c2243bc4
Guilherme <guilhermemachado755@gmail.com>
2023-03-14 10:18:06 -0300

    Fix:orders

--- user/urls.py
+++ user/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 from address.views import AddressView, AddressUpdateView
 from cart.views import CartView, CartDetailView
-
+from orders.views import OrderView
 from .views import UserView, UserDetailView, UserAccountRecoverView
 
 urlpatterns = [
     path("login/", jwt_views.TokenObtainPairView.as_view()),
     path("user/", UserView.as_view()),
     path("user/cart/", CartView.as_view()),
+    path("user/order/", OrderView.as_view()),
     path("user/address/", AddressView.as_view()),
     path("user/address/update/", AddressUpdateView.as_view()),
     path("user/<str:user_id>/", UserDetailView.as_view()),
